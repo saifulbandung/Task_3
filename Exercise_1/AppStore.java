ad5a269089406321213cd4c68eff386e260a64da
saifulbandung <saifullahbandung@yahoo.com>
2016-04-18 21:40:24 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,29 +1,29 @@
-//Nama	: Alim Nuriqra A
- //Nim	: 1301144032
- //Kelas	: IF-38-02
++//Nama	: Alim Nuriqra A
+ +//Nim	: 1301144032
+ +//Kelas	: IF-38-02
   
- public class AppStore{
- 	private Application[] appList = new Application[100];
- 	private int totalApp;
- 	
- 	public void createNewApp(String appName, int appSize){
- 		appList[totalApp] = new Application(appName,appSize);
- 		totalApp++;
- 	}
- 	
- 	public Application getApp(int id){
- 		return appList[id];
- 	}
- 	
- 	public int getTotalApp(){
- 		return totalApp;
- 	}
- 	
- 	public Application[] getAppList(){
- 		return appList;
- 	}
- 	
-	public String toString(){
- 		return "There are "+totalApp+" ready to instal";
- 	}
- }
+ +public class AppStore{
+ +	private Application[] appList = new Application[100];
+ +	private int totalApp;
+ +	
+ +	public void createNewApp(String appName, int appSize){
+ +		appList[totalApp] = new Application(appName,appSize);
+ +		totalApp++;
+ +	}
+ +	
+ +	public Application getApp(int id){
+ +		return appList[id];
+ +	}
+ +	
+ +	public int getTotalApp(){
+ +		return totalApp;
+ +	}
+ +	
+ +	public Application[] getAppList(){
+ +		return appList;
+ +	}
+ +	
+ +	public String toString(){
+ +		return "There are "+totalApp+" ready to instal";
+ +	}
+ +}
