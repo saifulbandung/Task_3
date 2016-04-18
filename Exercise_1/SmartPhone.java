751a98e22b18275920e4f020fb7903c09a6b144c
saifulbandung <saifullahbandung@yahoo.com>
2016-04-18 21:43:37 +0700

    Update SmartPhone.java

--- Exercise_1/SmartPhone.java
+++ Exercise_1/SmartPhone.java
@@ -1,1 +1,40 @@
-
++//Nama	: Akhmad Saifullah
+ +//Nim	: 1301144242
+ +//Kelas	: IF-38-02
+  
+ +public class SmartPhone{
+ +	private Application[] appList = new Application[10];
+ +	private int totalApp;
+ +	private int memory;
+ +	private int totalMemory;
+ +	
+ +	public void setMemory(int memory){
+ +		this.memory = memory;
+ +		totalMemory = this.memory;
+ +	}
+ +	
+ +	public void addApplication(AppStore appStore, int appId){
+ +		if(appStore.getApp(appId).getAppSize()<=getRemainingSize()){
+ +			appList[totalApp]=appStore.getApp(appId);
+ +			totalApp++;
+ +		}
+ +	}
+ +	
+ +	public Application getApp(int id){
+ +		return appList[id];
+ +	}
+ +	
+ +	public int getRemainingSize(){
+ +		int x=0;
+ +		for(int i=0;i<appList.length;i++){
+ +			if(appList[i]!=null){
+ +				x=x+appList[i].getAppSize();
+ +			}
+ +		}
+ +		return memory=memory-x;
+ +	}
+ +	
+ +	public String toString(){
+ +		return "memory size "+totalMemory+"MB, "+totalApp+" application installed, remaining memory "+memory;
+ +	}
+ +}
