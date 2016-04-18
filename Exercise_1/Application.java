f076616868cbe91eb2ce29fb29d1b663ba7147e1
saifulbandung <saifullahbandung@yahoo.com>
2016-04-18 21:42:11 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,25 @@
-
++//Nama	: Ahmad Saifullah
+ +//Nim	: 1301144242
+ +//Kelas	: IF-38-02
+  
+ +public class Application{
+ +	private String appName;
+ +	private int appSize;
+ +	
+ +	public Application(String appName, int appSize){
+ +		this.appName = appName;
+ +		this.appSize = appSize;
+ +	}
+ +	
+ +	public String getAppName(){
+ +		return appName;
+ +	}
+ +	
+ +	public int getAppSize(){
+ +		return appSize;
+ +	}
+ +	
+ +	public String toString(){
+ +		return "Application name : "+appName+" ,with size : "+appSize;
+ +	}
+ +}
